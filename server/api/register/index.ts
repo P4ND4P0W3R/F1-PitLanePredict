24c7f638677ff5ffbb1dd218e1e9157d783efdad
P4ND4P0W3R <paul.hu@efrei.net>
2023-12-05 13:41:57 +0100

    Fixed CORS issue

--- server/api/register/index.ts
+++ server/api/register/index.ts
@@ -2,28 +2,28 @@ import { db } from "../../prisma";
 import bcrypt from "bcrypt";
 
 export default defineEventHandler(async (event) => {
-    if (event.method === "OPTIONS") {
-        // set your options here
-        return null;
-    }
-    assertMethod(event, ["POST"]);
-    const body = await readBody(event);
-    console.log(body)
+  if (event.method === "OPTIONS") {
+    // set your options here
+    return null;
+  }
+  assertMethod(event, ["POST"]);
+  const body = await readBody(event);
+  console.log(body)
 
-    const { username, email, password } = body;
+  const { username, email, password } = body;
 
-    const hashedPassword = await bcrypt.hash(password, 10);
+  const hashedPassword = await bcrypt.hash(password, 10);
 
-    const users = await db.users.create({
-        data: {
-            username: username,
-            email: email,
-            password: hashedPassword,
-        },
-    });
+  const users = await db.users.create({
+    data: {
+      username: username,
+      email: email,
+      password: hashedPassword,
+    },
+  });
 
-    return {
-        status: 200,
-        body: JSON.stringify(users),
-    };
+  return {
+    status: 200,
+    body: JSON.stringify(users),
+  };
 });
